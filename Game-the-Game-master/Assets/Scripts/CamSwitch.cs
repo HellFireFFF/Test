a3c0008f162d4f00cae1be0d9c2b02922c698a63
HellFireFFF <77723563+HellFireFFF@users.noreply.github.com>
2021-02-02 14:55:53 +0100

    Test_1

---
+++ Game-the-Game-master/Assets/Scripts/CamSwitch.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamSwitch : MonoBehaviour
+{
+
+    public GameObject firstPersonCam;
+    public GameObject thirdPersonCam;
+
+    protected bool thirdPerson = true;
+
+    public void Start()
+    {
+
+        thirdPersonCam.SetActive(true);
+        firstPersonCam.SetActive(false);
+
+    }
+
+    public void FixedUpdate()
+    {
+
+        CameraSwitch();
+
+    }
+
+    public void CameraSwitch()
+    {
+
+        if(Input.GetKeyDown(KeyCode.X) && thirdPerson == true)
+        {
+
+            thirdPersonCam.SetActive(false);
+            firstPersonCam.SetActive(true);
+            thirdPerson = false;
+
+        }
+        else if(Input.GetKeyDown(KeyCode.X) && thirdPerson == false)
+        {
+
+            thirdPersonCam.SetActive(true);
+            firstPersonCam.SetActive(false);
+            thirdPerson = true;
+
+        }
+
+    }
+
+}
---
+++ Game-the-Game-master/Assets/Scripts/EnemyLookAtPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLookAtPlayer : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+
+        if (other.transform.tag == "Player")
+        {
+            //look at player
+            transform.LookAt(other.transform, Vector3.up);
+        }
+
+    }
+}
