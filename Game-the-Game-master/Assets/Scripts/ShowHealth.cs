1341d7a7897761b098293b103e8f8dbbc2e8f495
HellFireFFF <77723563+HellFireFFF@users.noreply.github.com>
2021-02-10 13:33:01 +0100

    Health "Bar"

---
+++ Game-the-Game-master/Assets/Scripts/ShowHealth.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowHealth : MonoBehaviour
+{
+
+    public Text ui;
+
+    protected string uiText = "";
+
+    public GameObject player;
+
+    protected float playerHealthRightNow;
+    protected string playerHealthRightNowText;
+
+    protected float playerHealthFull;
+    protected string playerHealthFullText;
+
+    public void Start()
+    {
+
+        
+
+        playerHealthRightNowText = playerHealthRightNow.ToString();
+        playerHealthFullText = playerHealthFull.ToString();
+        playerHealthRightNow = player.GetComponent<Stats>().currentHitpoints;
+        playerHealthFull = player.GetComponent<Stats>().baseHitpoints + player.GetComponent<Stats>().hitpointsBonus;
+
+     
+
+    }
+
+    public void Update()
+    {
+
+
+     
+        ShowHealthPlayer();
+        ui.text = uiText;
+
+    }
+
+    public void ShowHealthPlayer()
+    {
+
+       uiText = playerHealthRightNow + "/" + playerHealthFull;
+
+    }
+
+}
